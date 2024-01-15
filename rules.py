b9bedd44f63b34e35d843bd41e8ffe42012cac38
fukioston <2112966@mail.nankai.edu.cn>
2024-01-15 16:23:17 +0800

    添加视力标准

---
+++ rules.py
@@ -0,0 +1,6 @@
+class rules:
+    def __init__(self):
+        self.distance = 500  # cm
+        self.score = 4.0
+        self.allscore = [4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3]
+        self.size=[14, 12, 11, 10, 9, 8.5, 7.5, 6.6, 6, 5, 4, 3, 2, 1.5, 1]
