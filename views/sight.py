083417d193e96312e74af180fcab4209d51ee0e2
豁哈哈 <2963400300@qq.com>
2024-01-15 13:24:54 +0800

    //在前端显示E，随机生成E的方向，根据手势判断结果，正确则改变E的方向

--- views/sight.py
+++ views/sight.py
@@ -1,31 +1,34 @@
 import cv2 as cv2
 import mediapipe as mp
 import math
+import random
 import numpy
+
+
 class HandDetector:
 
     def __init__(self):
 
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(static_image_mode=False,
+        self.mpHands = mp.solutions.hands  # 初始化手部追踪模块
+        self.hands = self.mpHands.Hands(static_image_mode=False,  # 初始化手部追踪对象
                                         max_num_hands=2,
                                         min_detection_confidence=0.5,
                                         min_tracking_confidence=0.5)
-        self.mpDraw = mp.solutions.drawing_utils
-        self.capture = cv2.VideoCapture(0)
+        self.mpDraw = mp.solutions.drawing_utils  # 绘制手部关键点
+        self.capture = cv2.VideoCapture(0)  # 创建一个视频捕获对象
 
     def findDirection(self):
 
-        ret, img = self.capture.read()
+        ret, img = self.capture.read()  # 从视频中捕获一帧，ret表示帧是否捕获成功，img表示捕获得到的帧数据
 
         '''get results from the solution'''
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 将捕获的帧img的颜色空间从BGR转换为RGB
+        self.results = self.hands.process(imgRGB)  # 对处理后的帧进行手部追踪
 
         # '''
-        if self.results.multi_hand_landmarks:
-            for single_hand in self.results.multi_hand_landmarks:
-                self.mpDraw.draw_landmarks(img, single_hand, self.mpHands.HAND_CONNECTIONS)
+        if self.results.multi_hand_landmarks:  # 检测到手
+            for single_hand in self.results.multi_hand_landmarks:  # 遍历每一个手
+                self.mpDraw.draw_landmarks(img, single_hand, self.mpHands.HAND_CONNECTIONS)  # 绘制手部关节点间的连线
         # '''
 
         dirc = None
@@ -47,7 +50,7 @@ class HandDetector:
                                           lm_tip.y - lm_mcp.y,
                                           lm_tip.z - lm_mcp.z)
 
-                '''this condition test if the index finger is streched'''
+                '''this condition test if the index finger is stretched'''
                 if dist_tip2mcp > 1.8 * dist_pip2mcp:
 
                     '''the cosine of the angle between the vector from MCP to TIP and the positive direction of X/Y'''
@@ -65,10 +68,14 @@ class HandDetector:
                         dirc = 4
 
         return dirc
-def gen_frames0():
-    detector = HandDetector()
+
+
+detector_ = HandDetector()
+
+
+def gen_frames0(detector=detector_):
     while 1:
-        direction = detector.findDirection()
+        direction = detector.findDirection()  # 获得手的信息
         if direction is not None:
             print("Detected direction:", direction)
         # 显示图像（可选）
@@ -79,3 +86,6 @@ def gen_frames0():
         # 指定字节流类型image/jpeg
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
